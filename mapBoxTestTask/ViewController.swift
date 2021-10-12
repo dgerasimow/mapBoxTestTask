42e99ff619b00616b14147dff10c45f8757d65e3
thw1zard <68274539+thw1zard@users.noreply.github.com>
2021-10-12 15:07:23 +0300

    Initial Commit

---
+++ mapBoxTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mapBoxTestTask
+//
+//  Created by Danil Gerasimov on 12.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
